54e50c19dbe606578f189516e8668bee366a2a65
Miljan Pandurevic <miljan.pandurevic123@gmail.com>
2023-11-07 14:06:19 +1100

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+// const records = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+// ]
+
+function superbowlWin(record) {
+ const found =   record.find((element)=>{
+        return element.result === "W"
+    })
+
+    return found && found.year
+}
+
